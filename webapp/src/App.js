44020b5ad3e3f15a3c9b1f65d896262156882774
Jake Levine <jgl2832@gmail.com>
2020-02-11 13:58:59 +0100

    updated calendar and subtitle

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -6,7 +6,7 @@ function Calendar() {
     <div className="Calendar-container">
       <iframe
         title="nysilentscal"
-        src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=Z2V1c3F1NnY4czdrZ2Nlb2lsMm90ZG5rZmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23000000&amp;showTitle=1&amp;showNav=1&amp;showDate=1&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=1"
+        src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=cmpjb2xvNmpzcjlwMWNnNXNvZzEwZTNtZ29AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23000000&amp;showTitle=0&amp;showNav=1&amp;showDate=1&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=1"
         style={{borderWidth:0}}
         width="800"
         height="600"
@@ -24,6 +24,10 @@ function App() {
         <h1>
           NY Silents
         </h1>
+        <small className="App-header-subtitle">
+          A listing of silent film screenings and early cinema-related events happening in the NYC area
+        </small>
+        <br />
         <Calendar />
       </header>
     </div>
