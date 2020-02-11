945ff4a904cdb7553081dbfc71d9a00f2ea8da4b
Jake Levine <jgl2832@gmail.com>
2020-02-11 13:09:25 +0100

    fix build again

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -5,6 +5,7 @@ function Calendar() {
   return (
     <div className="Calendar-container">
       <iframe
+        title="nysilentscal"
         src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=Z2V1c3F1NnY4czdrZ2Nlb2lsMm90ZG5rZmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23000000&amp;showTitle=1&amp;showNav=1&amp;showDate=1&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=1"
         style={{borderWidth:0}}
         width="800"
