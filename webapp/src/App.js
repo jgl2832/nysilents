eb4222982b18b0c8398b65ab240c877b0c05a951
Jake Levine <jgl2832@gmail.com>
2020-02-11 12:31:39 +0100

    calendar exists

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import './App.css';
 
+function Calendar() {
+  const url = "https://calendar.google.com/calendar/embed?src=geusqu6v8s7kgceoil2otdnkfk%40group.calendar.google.com&ctz=Europe%2FBerlin"
+  return (
+    <iframe
+      src={url}
+      style={{border: 0}}
+      width="800"
+      height="600"
+      frameborder="0"
+      scrolling="no"
+    ></iframe>
+  );
+}
+
 function App() {
   return (
     <div className="App">
@@ -11,6 +25,7 @@ function App() {
         <p>
           Coming Soon
         </p>
+        <Calendar />
       </header>
     </div>
   );
