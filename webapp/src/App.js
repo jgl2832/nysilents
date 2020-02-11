526891d8b77613e1074109083c136ef64c656e25
Jake Levine <jgl2832@gmail.com>
2020-02-11 12:54:05 +0100

    make calendar a bit cleaner

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -2,12 +2,10 @@ import React from 'react';
 import './App.css';
 
 function Calendar() {
-  const url = "https://calendar.google.com/calendar/embed?src=geusqu6v8s7kgceoil2otdnkfk%40group.calendar.google.com"
   return (
     <iframe
-      title="nysilentscalendar"
-      src={url}
-      style={{border: 0}}
+      src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=Z2V1c3F1NnY4czdrZ2Nlb2lsMm90ZG5rZmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23000000&amp;showTitle=1&amp;showNav=1&amp;showDate=1&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=1"
+      style={{borderWidth:0}}
       width="800"
       height="600"
       frameborder="0"
