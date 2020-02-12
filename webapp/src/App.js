b9597068df75330ab6fbad33aafd33cc19e13950
Jake Levine <jgl2832@gmail.com>
2020-02-12 11:36:00 +0100

    banner color

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -6,8 +6,7 @@ function Calendar() {
     <div className="Calendar-container">
       <iframe
         title="nysilentscal"
-        src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=cmpjb2xvNmpzcjlwMWNnNXNvZzEwZTNtZ29AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23000000&amp;showTitle=0&amp;showNav=1&amp;showDate=1&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=1"
-        style={{borderWidth:0}}
+        src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=white&amp;src=cmpjb2xvNmpzcjlwMWNnNXNvZzEwZTNtZ29AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=black&amp;showTitle=0&amp;showNav=1&amp;showDate=1&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=1"
         width="800"
         height="600"
         frameBorder="0"
